27ab1b42482ee5a60a38b9492ea48e9fe45c697a
sarath <sarathos584@gmail.com>
2024-12-18 20:23:00 +0530

    style: reduce header padding

--- quiz-react/src/components/Header.tsx
+++ quiz-react/src/components/Header.tsx
@@ -1,9 +1,9 @@
 
 
 const Header = () => {
-    
+
   return (
-    <header className="py-[3em]">
+    <header className="py-[2em]">
         <div className="mx-auto bg-slate-100 top-11 max-w-[80vw] md:max-w-[60vw] lg:max-w-[40vw] p-3 sm:text-xl md:text-2xl lg:text-3xl rounded-md">
             <h1 className="text-red-500 font-medium text-center">Quiz Application UI</h1>
         </div>
