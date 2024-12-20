aa75df5332ff8b8814fe7401dc378811b909f658
sarath <sarathos584@gmail.com>
2024-12-20 10:56:27 +0530

    refactor: make header responsive

--- quiz-react/src/components/Header.tsx
+++ quiz-react/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
 
   return (
     <header className="py-[2em]">
-        <div className="mx-auto bg-slate-100 top-11 max-w-[80vw] md:max-w-[60vw] lg:max-w-[40vw] p-3 sm:text-xl md:text-2xl lg:text-3xl rounded-md">
+        <div className="mx-auto bg-slate-100 top-11 max-w-[30vw] mobile:max-w-[60vw] tablet:max-w-[50vw] p-3 sm:text-xl md:text-2xl lg:text-3xl rounded-md">
             <h1 className="text-red-500 font-medium text-center">Quiz Application UI</h1>
         </div>
     </header>
