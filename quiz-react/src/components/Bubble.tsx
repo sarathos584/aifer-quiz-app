e7647d8c6b75b7d99e4ae9b319c5280f1fdde513
sarath <sarathos584@gmail.com>
2025-01-02 10:30:26 +0530

    fix: issue with question bubble background on toggle. remove unwanted imports

--- quiz-react/src/components/Bubble.tsx
+++ quiz-react/src/components/Bubble.tsx
@@ -1,5 +1,7 @@
+'use client'
 
-import { memo } from "react"
+import { useQuestionContext } from "@/contexts/QuestionsProvider"
+import { memo, useCallback, useEffect } from "react"
 
 const statusStyleObj: { [key: string]: string } = {
     active: 'bg-indigo-400 text-white',
@@ -19,8 +21,24 @@ const Bubble = ({ value, status, id }: Props) => {
     return statusStyleObj[status] ? statusStyleObj[status] : 'bg-slate-300 text-black' 
   }
 
+  const { questionStatusToggle, selectedAnswer, activeQuestion, questions } = useQuestionContext()
+
+
+  useEffect(()=> {
+    if (questions[activeQuestion]?.selectedAnswer && value === activeQuestion) {
+      questionStatusToggle(value, 'attended')
+    }
+  }, [activeQuestion, questionStatusToggle, questions, selectedAnswer, value])
+
+  const selectQuestionOnClick = useCallback(()=> {
+
+      questionStatusToggle(value, '')
+  }, [questionStatusToggle, value])
+
   return (
-    <div className={`w-[2.5em] h-[2.5em] cursor-pointer rounded-full p-2  grid place-content-center ${getAdditionalStyles()}`}>
+    <div 
+    onClick={selectQuestionOnClick}
+    className={`w-[2.5em] h-[2.5em] cursor-pointer rounded-full p-2  grid place-content-center ${getAdditionalStyles()}`}>
         <span>{value + 1}</span>
     </div>
   )
